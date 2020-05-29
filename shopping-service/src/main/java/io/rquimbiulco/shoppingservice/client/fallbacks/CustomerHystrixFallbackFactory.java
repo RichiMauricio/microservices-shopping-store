323914ed64748830a1802d5f6a05160c91ffcdf2
rquimbiulco <richimaurok@gmail.com>
2020-05-29 14:17:34 -0500

    hystrix implementation

---
+++ shopping-service/src/main/java/io/rquimbiulco/shoppingservice/client/fallbacks/CustomerHystrixFallbackFactory.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package io.rquimbiulco.shoppingservice.client.fallbacks;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import io.rquimbiulco.shoppingservice.client.CustomerClient;
+import io.rquimbiulco.shoppingservice.model.Customer;
+
+/**
+ * @author Richard
+ *
+ */
+@Component
+public class CustomerHystrixFallbackFactory implements CustomerClient {
+
+	@Override
+	public ResponseEntity<Customer> getCustomer(long id) {
+		Customer customer = Customer.builder().firstName("none").lastName("none").email("none").photoUrl("none")
+				.build();
+		return ResponseEntity.ok(customer);
+	}
+
+}
